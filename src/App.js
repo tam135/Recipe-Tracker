64ff8b79252d915537fa7060b256a46f07dd802c
Tam Phan <tnphan0204@gmail.com>
2023-02-22 13:59:42 -0500

    Added create and delete recipe function to App componenet

--- src/App.js
+++ src/App.js
@@ -6,14 +6,29 @@ import RecipeView from "./RecipeView";
 
 
 function App() {
+  // State for recipes
+  const [recipes, setRecipes] = useState([]);
 
+  // Create new recipe function
+  const createRecipe = (newRecipe) => {
+    setRecipes((currentRecipes) => [
+      newRecipe,
+      ...currentRecipes,     
+    ])
+  }
 
+  // Delete recipe function
+  const deleteRecipe = (indexToDelete) => {
+    setRecipes((currentRecipes) => {
+      currentRecipes.filter((recipe, index) => index !== indexToDelete)
+    })
+  }
   
   return (
     <>  
       <Header/>
-      <RecipeCreate/>
-      <RecipeView/>
+      <RecipeCreate createRecipe={createRecipe}/>
+      <RecipeView recipes={recipes} deleteRecipe={deleteRecipe}/>
     </>
   );
 }
